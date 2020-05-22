79d7be6f28d88d6f549662f2ec6ab34ee43e401e
KSClopton <kyle.clopton@outlook.com>
2020-05-22 11:17:04 -0400

    Added components, and set up initial states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+// Import go here
+import React, {useState, useEffect} from "react";
+import {BrowserRouter as Router, Links, Route} from 'react-router-dom';
+import Nav from './Components/Nav'
+import Home from './Components/Home'
+import PizzaBuild from './Components/PizzaBuild'
+import Order from './Components/Order'
+
+// Adding initial states here
+const initialPizza = {}
+const initialErrors = {}
+
 
 const App = () => {
+  // Adding states here
+  const [,] = useState()
+  const [,] = useState()
+
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+    <div>
+      <Nav />
+      <Route exact path='/Home' component={Home} />
+      <Route exact path='/Home' component={PizzaBuild}/>
+      <Route exact path='/Home' component={Order}/>
+    </div>
+    </Router>
   );
 };
 export default App;
